87d23cf24ade80e14b3354ad8e38166b6b47eb7a
Jeremy Simkins <jpSimkins@gmail.com>
2015-03-02 15:28:06 -0500

    Removed BAK files form repo...
    didn't mean to push these...

--- drupal2wpBAK/views/step3.php
+++
@@ -1,6 +0,0 @@
-<h3 style="margin: 0;"><?php _e('Import in Progress', 'drupal2wp'); ?></h3>
-<p class="description"><?php _e('This can take a long time depending on the size of the Drupal database.', 'drupal2wp'); ?></p>
-
-<p><?php _e('Import status will be outputted below.', 'drupal2wp'); ?></p>
-
-<?php do_action('drupal2wp_import_iframe'); ?>
